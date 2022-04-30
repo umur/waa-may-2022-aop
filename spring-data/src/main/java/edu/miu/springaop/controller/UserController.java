6eee7d4db9a1ba58b4c3a96c681a4d0acb503c4e
Meresa <meresa27@gmail.com>
2022-04-29 20:54:03 -0500

    execution time annotation added to methods in controller

--- spring-data/src/main/java/edu/miu/springaop/controller/UserController.java
+++ spring-data/src/main/java/edu/miu/springaop/controller/UserController.java
@@ -1,6 +1,7 @@
 package edu.miu.springaop.controller;
 
 import edu.miu.springaop.DTO.UserDto;
+import edu.miu.springaop.aspect.annotation.ExecutionTime;
 import edu.miu.springaop.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,19 +17,24 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ExecutionTime
     @PostMapping
     public void save(@RequestBody UserDto u) {
         userService.save(u);
     }
+    @ExecutionTime
     @GetMapping
     public List<UserDto> getAll() {
         return userService.getAll();
     }
+
+    @ExecutionTime
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getById(@PathVariable int id) {
         return ResponseEntity.ok(userService.getById(id));
     }
 
+    @ExecutionTime
     @DeleteMapping("/{id}")
     public void delete(@PathVariable int id) {
         userService.deleteById(id);
