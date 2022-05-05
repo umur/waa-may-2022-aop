750f35158f392f22ca1b8a8f7fdee753a55c453a
MaherFayezk <92121234+MaherFayezk@users.noreply.github.com>
2022-05-05 15:10:29 -0500

    done

--- src/main/java/com/example/demo/repo/ActivityLogRepo.java
+++ src/main/java/com/example/demo/repo/ActivityLogRepo.java
@@ -2,6 +2,8 @@ package com.example.demo.repo;
 
 import com.example.demo.entity.ActivityLog;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivityLogRepo extends CrudRepository<ActivityLog,Integer> {
 }
